12a9e170adfacd685caae4a9466bbfd4a5412518
Karly Peckham <kpeckham@southern.edu>
2018-04-15 19:59:34 -0400

    first commit

---
+++ FirstLinkGetter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+
+public class FirstLinkGetter {
+
+    public static void main() {
+        string path = "~/put path here.txt";
+        using (StreamWriter streamWriter = new StreamWriter(path)) {
+            
+            using (StreamReader streamReader = new StreamReader("enwiki20180401-pages-articles.xml")) {
+                while (streamReader.Peek() > -1) {
+                    string line = streamReader.ReadLine();
+
+                }
+            }
+        }
+             
+    }
+}
