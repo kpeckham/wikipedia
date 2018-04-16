a4b128640336f399a87f6f2b50eea7067405e843
Karly Peckham <kpeckham@southern.edu>
2018-04-15 21:33:40 -0400

    added info to METADATA

--- FirstLinkGetter/FirstLinkGetter.cs
+++ FirstLinkGetter/FirstLinkGetter.cs
@@ -7,44 +7,51 @@ public class FirstLinkGetter {
     enum StateOptions { FINDFIRST, METADATA, REDIRECT, SKIPPAGE, TEXT }; 
 
     public static void Main() {
-        string path = "~/put path here.txt";
-        //using (StreamWriter streamWriter = new StreamWriter(path)) {
+        string home = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+        string path = home + "/firstLinks1.txt";
+        using (StreamWriter streamWriter = new StreamWriter(path)) {
 
-            string home = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
 
             using (StreamReader streamReader = new StreamReader(home + "/enwiki-20180401/enwiki-20180401-pages-articles.xml")) {
 
                 StateOptions state = StateOptions.FINDFIRST;
                 string line;
+                string id;
+                string link;
                 while ((line = streamReader.ReadLine()) != null) {
                     switch (state) {
-                        case StateOptions.FINDFIRST: {
+                        case StateOptions.FINDFIRST: 
                             
                             if (line == "  <page>") {
                                 state = StateOptions.METADATA;
                             }
                             break;
-                        }
-                        case StateOptions.METADATA: {
+                        
+                        case StateOptions.METADATA:
 
-                            Console.WriteLine(line);
-                            Environment.Exit(0);
+                            if (line.StartsWith("    <id>", StringComparison.CurrentCulture)) {
+                                id = line.Substring(8, line.Length - 8 - 4);
+                                Console.WriteLine(id);
+                                Environment.Exit(0);
+
+                            }
+
                             break;
-                        }
-                        case StateOptions.REDIRECT: {
+                        
+                        case StateOptions.REDIRECT: 
                             break;
-                        }
-                        case StateOptions.SKIPPAGE: {
+                        
+                        case StateOptions.SKIPPAGE: 
                             break;
-                        }
-                        case StateOptions.TEXT: {
+                        
+                        case StateOptions.TEXT: 
                             break;
-                        }
+                        
 
                     }
                 }
             }
-        //}
+        }
              
     }
 }
