b3a1f5f4db9e8f88fc05e8e8f9aeb77c53a62ce1
Karly Peckham <kpeckham@southern.edu>
2018-04-15 20:45:04 -0400

    basic trial

--- FirstLinkGetter.cs
+++ FirstLinkGetter.cs
@@ -4,17 +4,36 @@ using System.IO;
 
 public class FirstLinkGetter {
 
-    public static void main() {
-        string path = "~/put path here.txt";
-        using (StreamWriter streamWriter = new StreamWriter(path)) {
-            
-            using (StreamReader streamReader = new StreamReader("enwiki20180401-pages-articles.xml")) {
-                while (streamReader.Peek() > -1) {
-                    string line = streamReader.ReadLine();
+    enum StateOptions { FINDFIRST, METADATA, REDIRECT, SKIPPAGE, TEXT }; 
 
-                }
+    public static void Main() {
+        string path = "~/put path here.txt";
+        //using (StreamWriter streamWriter = new StreamWriter(path)) {
+            
+            using (StreamReader streamReader = new StreamReader("~/enwiki-20180401/enwiki-20180401-pages-articles.xml")) {
+                string line = streamReader.ReadLine();
+                Console.WriteLine(line);
+                //StateOptions state = StateOptions.FINDFIRST;
+                //while (true) {
+                //    switch (state) {
+                //        case StateOptions.FINDFIRST {
+                //        }
+                //        case StateOptions.METADATA {
+
+                //        }
+                //        case StateOptions.REDIRECT {
+
+                //        }
+                //        case StateOptions.SKIPPAGE {
+
+                //        }
+                //        case StateOptions.TEXT {
+
+                //        }
+                //    }
+                //}
             }
-        }
+        //}
              
     }
 }
