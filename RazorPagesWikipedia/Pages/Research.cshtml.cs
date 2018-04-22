6d84cd6e5c9ba4b5e0379cc01a17a69f3987c28c
Karly Peckham <kpeckham@southern.edu>
2018-04-22 14:42:13 -0400

    added part of a recursive search function

--- RazorPagesWikipedia/Pages/Research.cshtml.cs
+++ RazorPagesWikipedia/Pages/Research.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesWikipedia.DbModels;
+using System.Text;
 
 namespace RazorPagesWikipedia.Pages
 {
@@ -14,13 +15,57 @@ namespace RazorPagesWikipedia.Pages
         {
         }
 
-        public int CountPages() 
+        public int CountPages()
         {
-            using (var db = new WikiDbContext()) {
+            using (var db = new WikiDbContext())
+            {
                 return db.Page.Count();
+
                 // can give count a lambda or could do .Select().Count()
             }
 
+        }
+
+        public void loopPages() {
+            Dictionary<int, FirstLinkInfo> ToPhilosophy = new Dictionary<int, FirstLinkInfo>();
+            using (var db = new WikiDbContext()) {
+                byte[] compareText = Encoding.UTF8.GetBytes("Featured_articles");
+                var pages = db.Categorylinks.Where(cl => cl.ClTo == compareText);
+
+                foreach (var page in pages) 
+                {
+                    if (!ToPhilosophy.ContainsKey((int)page.ClFrom))
+                    {
+                        ToPhilosophy.Add((int)page.ClFrom, null);
+                        FindPhilosophy((int)page.ClFrom);
+                    }
+                    else
+                    {
+                        FirstLinkInfo info = ToPhilosophy.GetValueOrDefault((int)page.ClFrom);
+                        
+                    }
+                }
+            }
+        }
+
+        public int FindPhilosophy(int FromId) {
+            
+        }
+    }
+
+    public class FirstLinkInfo
+    {
+        public int Depth;
+        public bool GoesToPhilosophy;
+        public bool InALoop;
+        public int PageId;
+
+        public FirstLinkInfo(int PageId, bool GoesToPhilosophy, bool InALoop, int Depth) 
+        {
+            this.Depth = Depth;
+            this.PageId = PageId;
+            this.InALoop = InALoop;
+            this.GoesToPhilosophy = GoesToPhilosophy;
         }
     }
 }
