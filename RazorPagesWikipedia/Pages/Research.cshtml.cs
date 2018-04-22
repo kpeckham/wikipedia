4673fe8268dec9d043ac1ec098ecc19088cbf481
Karly Peckham <kpeckham@southern.edu>
2018-04-21 22:00:12 -0400

    added basic Research page

---
+++ RazorPagesWikipedia/Pages/Research.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RazorPagesWikipedia.Pages
+{
+    public class ResearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public int CountPages() 
+        {
+            return 1; 
+        }
+    }
+}
